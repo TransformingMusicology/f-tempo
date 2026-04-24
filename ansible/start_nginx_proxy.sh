f5bf167d8b58042b82b5ec2c1e1e26d94bf7e13e
Alastair Porter <alastair@porter.net.nz>
2026-04-24 14:55:41 +0200

    Update docker-compose.yml and nginx-proxy script to match current prod infra

--- ansible/start_nginx_proxy.sh
+++ ansible/start_nginx_proxy.sh
@@ -1,6 +1,6 @@
 # Start nginx-proxy and acme-companion to set up https certificates automatically using letsencrypt
 
-sudo docker run --detach \
+docker run --detach \
     --name nginx-proxy \
     --publish 80:80 \
     --publish 443:443 \
@@ -8,11 +8,14 @@ sudo docker run --detach \
     --volume vhost:/etc/nginx/vhost.d \
     --volume html:/usr/share/nginx/html \
     --volume /var/run/docker.sock:/tmp/docker.sock:ro \
+    --restart always \
     nginxproxy/nginx-proxy
 
-sudo docker run --detach \
-    --name nginx-proxy-acme \
-    --volume /var/run/docker.sock:/var/run/docker.sock:ro \
-    --volume acme:/etc/acme.sh \
-    --env "DEFAULT_EMAIL=mail@yourdomain.tld" \
-    nginxproxy/acme-companion
+docker run --detach \
+	--name nginx-proxy-acme \
+    	--volumes-from nginx-proxy \
+    	--volume /var/run/docker.sock:/var/run/docker.sock:ro \
+    	--volume acme:/etc/acme.sh \
+    	--env "DEFAULT_EMAIL=mail@yourdomain.tld" \
+	--restart always \
+    	nginxproxy/acme-companion
